9ceb5b10c7991dd72d266510cb14220d804764cc
Limon <valerijf@stud.ntnu.no>
2015-02-10 11:48:13 +0100

    Added TaskAdministrator

---
+++ src/Excercise2/TaskAdministrator.java
@@ -0,0 +1,10 @@
+package Excercise2;
+
+import jade.Boot;
+
+
+public class TaskAdministrator extends jade.core.Agent {
+    public static void main(String args[]) {
+        Boot.main(new String[]{"-gui", "TA:Excercise2.TaskAdministrator()"});
+    }
+}
