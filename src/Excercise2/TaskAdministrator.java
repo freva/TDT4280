8ea9ae761e363cc3a7d6ae28879f36face2b7b71
Brage Jahren <brage.jahren@gmail.com>
2015-02-10 16:13:03 +0100

    Added additionAgents to "yellow pages". Admin finds them

--- src/Excercise2/TaskAdministrator.java
+++ src/Excercise2/TaskAdministrator.java
@@ -3,18 +3,45 @@ package Excercise2;
 import Excercise2.parser.Parser;
 import Excercise2.parser.Node;
 import jade.Boot;
+import jade.core.AID;
 import jade.core.behaviours.CyclicBehaviour;
+import jade.core.behaviours.TickerBehaviour;
+import jade.domain.DFService;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
+import jade.domain.FIPAException;
 import jade.lang.acl.ACLMessage;
 
 
 public class TaskAdministrator extends jade.core.Agent {
     public static void main(String args[]) {
-        Boot.main(new String[]{"-gui", "TA:Excercise2.TaskAdministrator()"});
+        Boot.main(new String[]{"-gui", "TA:Excercise2.TaskAdministrator;AA:Excercise2.agents.AdditionAgent;AA2:Excercise2.agents.AdditionAgent"});
 
     }
 
 
     protected void setup() {
+
+        addBehaviour(new TickerBehaviour(this, 15000) {
+            protected void onTick() {
+                DFAgentDescription template = new DFAgentDescription();
+                ServiceDescription sd = new ServiceDescription();
+                sd.setType("Addition");
+                template.addServices(sd);
+                try {
+                    DFAgentDescription[] result = DFService.search(myAgent, template);
+                    System.out.println("Found the following addition agents:");
+                    AID [] sellerAgents = new AID[result.length];
+                    for (int i = 0; i < result.length; ++i) {
+                        sellerAgents[i] = result[i].getName();
+                        System.out.println(sellerAgents[i].getName());
+                    }
+                }
+                catch (FIPAException fe) {
+                    fe.printStackTrace();
+                }
+            }
+        } );
         addBehaviour(new CyclicBehaviour(this) {
             public void action() {
                 ACLMessage msg = receive();
--- src/Excercise2/agents/AdditionAgent.java
+++ src/Excercise2/agents/AdditionAgent.java
@@ -3,18 +3,35 @@ package Excercise2.agents;
 import Excercise2.parser.Node;
 import jade.core.Agent;
 import jade.core.behaviours.CyclicBehaviour;
+import jade.domain.DFService;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
+import jade.domain.FIPAException;
 import jade.lang.acl.UnreadableException;
 
 public class AdditionAgent extends Agent {
     protected void setup() {
-        addBehaviour(new CyclicBehaviour(this) {
-            public void action() {
-                try {
-                    Node msg = (Node) receive().getContentObject();
-                } catch (UnreadableException e) {
-                    e.printStackTrace();
-                }
-            }
-        });
+
+        DFAgentDescription dfd = new DFAgentDescription();
+        dfd.setName(getAID());
+        ServiceDescription sd = new ServiceDescription();
+        sd.setType("Addition");
+        sd.setName(getLocalName());
+        dfd.addServices(sd);
+        try {
+            DFService.register(this, dfd);
+        }
+        catch (FIPAException fe) {
+            fe.printStackTrace();
+        }
+//        addBehaviour(new CyclicBehaviour(this) {
+//            public void action() {
+//                try {
+//                    Node msg = (Node) receive().getContentObject();
+//                } catch (UnreadableException e) {
+//                    e.printStackTrace();
+//                }
+//            }
+//        });
     }
 }
