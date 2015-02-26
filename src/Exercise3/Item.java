4edbea20cc1ef5ea26ef1511ba485c8239acc97c
Limon <valerijf@stud.ntnu.no>
2015-02-26 12:53:21 +0100

    Added value to item

--- src/Exercise3/Item.java
+++ src/Exercise3/Item.java
@@ -1,14 +1,19 @@
 package Exercise3;
 
 public enum Item {
-    bronze(0), iron(1), steel(2), silver(3), gold(4), platinum(5), titanium(6), uranium(7);
+    bronze(0, 10), iron(1, 15), steel(2, 20), silver(3, 25), gold(4, 30), platinum(5, 35), titanium(6, 40), uranium(7, 50);
 
-    private int id;
-    private Item(int id){
+    private int id, value;
+    private Item(int id, int value){
         this.id = id;
+        this.value = value;
     }
 
     public int getId() {
         return id;
+    }
+
+    public int getValue() {
+        return value;
     }
 }
