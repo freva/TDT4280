e3baa175f32bc870441e06024abf0b7f36f9f05b
Limon <valerijf@stud.ntnu.no>
2015-02-27 11:35:20 +0100

    Renamed items

--- src/Exercise3/containers/Item.java
+++ src/Exercise3/containers/Item.java
@@ -1,7 +1,7 @@
 package Exercise3.containers;
 
 public enum Item {
-    bronze(0, 10), iron(1, 15), steel(2, 20), silver(3, 25), gold(4, 30), platinum(5, 35), titanium(6, 40), uranium(7, 50);
+    BRONZE(0, 10), IRON(1, 15), STEEL(2, 20), SILVER(3, 25), GOLD(4, 30), COPPER(5, 35), PLATINUM(6, 40), TITANIUM(7, 50);
 
     private int id, value;
     private Item(int id, int value){
