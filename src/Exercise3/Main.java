b8c2d9fea02eec4bb4d3a834003f6735efdcd498
Limon <valerijf@stud.ntnu.no>
2015-02-27 12:26:06 +0100

    Exchange requires number of agents argument

--- src/Exercise3/Main.java
+++ src/Exercise3/Main.java
@@ -8,10 +8,10 @@ public class Main {
     public static final int numAgents = 3;
 
     public static void main(String[] args) {
-        String boot[] = new String[]{"-gui", "Ex:Exercise3.Exchange"};
+        String boot[] = new String[]{"-gui", "Ex:Exercise3.Exchange(" + numAgents + ")"};
 
         for(int i=0; i<numAgents; i++) {
-            boot[1] += ";NG" + i + ":Exercise3.NegotiationAgent(" + i + ")";
+            boot[1] += ";NG" + i + ":Exercise3.NegotiationAgent";
         }
 
         System.out.println(Arrays.toString(boot));
